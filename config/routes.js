4d0dc1ce641770388e38ce9f7952fc1ac6462c80
Sean.Rennie <sean.rennie6@gmail.com>
2018-07-30 13:32:09 +0100

    added route security middleware

--- config/routes.js
+++ config/routes.js
@@ -7,6 +7,20 @@ const sessionController = require('../controllers/sessionController');
 const geoSiteController = require('../controllers/geoSiteController');
 const reviewController = require('../controllers/reviewController');
 
+//SECURE ROUTES
+function secureRoute(req, res, next){
+  //check if user is logged in, send message if note
+  if(!req.session.userId){
+    //user is not logged in Disallow!
+    return req.session.regenerate(() => {
+      //req.flash('danger', 'Log in to make edits!');
+      res.redirect('/session/new');
+    });
+  }
+
+  return next();
+}
+
 //GLOBAL ROUTES
 router.route('/')
   .get( (req, res) => res.render('pages/home'));
@@ -29,22 +43,22 @@ router.route('/session')
   .post(sessionController.create);     //to create a new user cookie
 
 router.route('/session/delete')
-  .get(sessionController.delete); //to regenerate a cookie
+  .get(secureRoute, sessionController.delete); //to LOGOUT and regenerate a cookie
 
 //  --> GeoSites
 router.route('/geoSites')
-  .get(geoSiteController.index)
-  .post(geoSiteController.create);
+  .get(geoSiteController.index)   //view all geosites
+  .post(secureRoute, geoSiteController.create); //Add a new site
 
 router.route('/geoSites/new')
-  .get(geoSiteController.new);
+  .get(secureRoute, geoSiteController.new);  //Add a new site
 
 router.route('/geoSites/:id')
   .get(geoSiteController.show);
 
 //  --> Site Reviews
 router.route('/geoSites/:siteId/review/new')
-  .get(reviewController.new);
+  .get(secureRoute, reviewController.new);
 
 // router.route('/geoSite/:siteId/review')
 //   .post(reviewController.create);
