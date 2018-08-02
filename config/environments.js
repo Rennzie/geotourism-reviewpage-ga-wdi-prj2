3e7eb4a67448a7bcdcc4ff62c9e95d4efceee701
Sean.Rennie <sean.rennie6@gmail.com>
2018-08-02 12:10:20 +0100

    update package.json for Heroku

--- config/environments.js
+++ config/environments.js
@@ -1,5 +1,5 @@
-const DB_URI = 'mongodb://localhost/nice-rocks'; //the address to the mongoDataBase
-const PORT = 8000;
+const DB_URI = process.env.MONGODB_URI || 'mongodb://localhost/nice-rocks'; //the address to the mongoDataBase
+const PORT = process.env.PORT || 8000;
 const GE_API = 'AIzaSyAZ_QH7WUOknfpY-UmWGxq19xO7kqysh0o';
 
 module.exports = {
