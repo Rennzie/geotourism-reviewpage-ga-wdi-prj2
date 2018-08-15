820d1813f50c56c51534ba9fc5146eac3f161dda
Sean.Rennie <sean.rennie6@gmail.com>
2018-08-15 08:39:30 +0100

    removed the API key from the visible code

--- config/environments.js
+++ config/environments.js
@@ -1,6 +1,6 @@
 const DB_URI = process.env.MONGODB_URI || 'mongodb://localhost/nice-rocks'; //the address to the mongoDataBase
 const PORT = process.env.PORT || 8000;
-const GE_API = 'AIzaSyAZ_QH7WUOknfpY-UmWGxq19xO7kqysh0o';
+const GE_API  = process.env.GOOGLE_EARTH_API_KEY;
 
 module.exports = {
   DB_URI, PORT, GE_API
